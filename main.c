549c08dc77386e63df804ce79c88b683a79721be
rafa <rafatest@gmail.com>
2024-11-30 22:58:24 +0100

    .

--- main.c
+++ main.c
@@ -2,9 +2,14 @@
 
 int main()
 {
+	/*
 	int tst = -123188;
 	int m = ft_printf("the number is %x\n", tst);
 	ft_printf("Number of characthers printed: %d\n", m);
 	int n = printf("the number is %x\n", tst);
 	printf("Number of characters printed: %d\n", n);
+	*/
+
+	int j = printf(NULL);
+	printf("%d", j);
 }
