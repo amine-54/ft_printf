856917f26ca6f4b9e96cf1030e7c8fc094f45cc6
amine-54 <m.manyani0@gmail.com>
2024-11-30 23:13:45 +0100

    .

--- ft_printf.h
+++ ft_printf.h
@@ -11,9 +11,9 @@
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF_H
-#define FT_PRINTF_H
+# define FT_PRINTF_H
 
-#include <unistd.h>
+# include <unistd.h>
 #include <stdio.h>
 #include <stdarg.h>
 
