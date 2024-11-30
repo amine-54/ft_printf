1346751499abb4bc5a6c29dc0700dd739989264b
Mohamed Amine Manyani <mmanyani@c3r9p4.1337.ma>
2024-11-30 12:36:40 +0100

    .

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: mmanyani <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/30 09:42:29 by mmanyani          #+#    #+#             */
-/*   Updated: 2024/11/30 10:01:21 by mmanyani         ###   ########.fr       */
+/*   Updated: 2024/11/30 12:29:29 by mmanyani         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,32 +14,43 @@
 #include <stdarg.h>
 #include <stdio.h>
 
-void	ft_putnbr(int n);
+int	ft_putnbr(int n);
 
-void	print_char(char c)
+int	print_char(char c)
 {
 	write(1, &c, 1);
+	return (1);
 }
 
-void	print_string(char *s)
+int	print_string(char *s)
 {
+	int	count;
+	
+	count = 0;
 	while(*s)
 	{
 		write(1, s, 1);
+		count++;
 		s++;
 	}
+	return(count);
 }
 
-void print_memory(unsigned long mem)
+int	print_memory(unsigned long mem)
 {
+	int	count;
 	char	*hex;
 	char	hex_mem[16];
 	int	i;
 
+	count = 0;
 	i = 15;
 	hex = "0123456789abcdef";
 	if (mem == 0)
+	{
 		write(1, "0x0", 3);
+		return (3);
+	}
 	hex_mem[i] = '\0';
 	i--;
 	while (mem > 0)
@@ -49,14 +60,51 @@ void print_memory(unsigned long mem)
 		i--;
 	}
 	write(1, "0x", 2);
+	count = count + 2;
 	write(1, &hex_mem[i + 1], 15 - i);
+	count = count + 15 - i;
+	return (count);
 }
 
-void	ftttt_printf(const char *format, ...)
+int	print_unsigned(unsigned int n)
+{
+	int	count;
+
+	count = 0;
+	if (n >= 10)
+		count = count + print_unsigned(n / 10);
+	char c = n % 10 + 48;
+	write(1, &c, 1);
+	count++;
+	return (count);
+}
+
+int	print_hex(unsigned int n , char c)
+{
+	int	count;
+
+	count = 0;
+	char *hex;
+	if (c == 'x')
+		hex = "0123456789abcdef";
+	else if (c == 'X')
+		hex = "0123456789ABCDEF";
+	else
+		return (0);
+	if (n >= 16)
+		count = count + print_hex(n / 16, c);
+	write(1, &hex[n % 16], 1);
+	count++;
+	return(count);
+}
+		
+int	ftttt_printf(const char *format, ...)
 {
 	va_list	args;
 	int	i;
-	
+	int	total;
+
+	total = 0;
 	i = 0;
 	va_start(args, format);
 	while (format[i])
@@ -65,14 +113,19 @@ void	ftttt_printf(const char *format, ...)
 		{
 			i++;
 			if (format[i] == 'c')
-				print_char((char)va_arg(args, int));
+				total = total + print_char((char)va_arg(args, int));
 			else if (format[i] == 's')
-				print_string(va_arg(args, char*));
+				total = total + print_string(va_arg(args, char*));
 			else if (format[i] == 'p')
-				print_memory((unsigned long)va_arg(args, void*));
+				total = total + print_memory((unsigned long)va_arg(args, void*));
 			else if (format[i] == 'd' || format[i] == 'i')
-				ft_putnbr(va_arg(args, int));
-
+				total = total + ft_putnbr(va_arg(args, int));
+			else if (format[i] == 'u')
+				total = total + print_unsigned(va_arg(args, unsigned int));
+			else if (format[i] == 'x' || format[i] == 'X')
+				total = total + print_hex(va_arg(args, unsigned int), format[i]);
+			else if (format[i] == '%')
+				total = total + print_char(format[i]);
 
 		}
 		else
@@ -80,10 +133,13 @@ void	ftttt_printf(const char *format, ...)
 		i++;
 	}
 	va_end(args);
+	return (total);
 } 
 
 int main()
 {
-	int c = 1578132;
-	ftttt_printf("tstalah %i ghjgy\n", c);
+	int m = ftttt_printf("the number is %d\n", 42);
+	ftttt_printf("Number of characthers printed: %d\n", m);
+	int n = printf("the number is %d\n", 42);
+	printf("Number of characters printed: %d\n", n);
 }
--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,27 +6,34 @@
 /*   By: mmanyani <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/30 09:42:21 by mmanyani          #+#    #+#             */
-/*   Updated: 2024/11/30 09:53:39 by mmanyani         ###   ########.fr       */
+/*   Updated: 2024/11/30 12:34:53 by mmanyani         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void	ft_putnbr(int n)
+int	ft_putnbr(int n)
 {
+	int	count;
+
+	count = 0;
 	if (n == -2147483648)
 	{
 		write(1, "-", 1);
 		write(1, "2", 1);
+		count = 2;
 		ft_putnbr(147483648);
 	}
 	if (n < 0)
 	{
 		write(1, "-", 1);
+		count++;
 		n = -n;
 	}
 	if (n >= 10)
-		ft_putnbr(n / 10);
+		count = count + ft_putnbr(n / 10);
 	char m = n % 10 + 48;
 	write(1, &m, 1);
+	count++;
+	return (count);
 }
